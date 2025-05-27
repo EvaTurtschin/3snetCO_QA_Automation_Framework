aa1ec3cac57a51dcfaac1fd61d3889710046750f
AIT TR Student <evgeniya@turtschin@gmail.com>
2025-05-27 19:28:50 +0200

    Test passed:affiliateProgramsPageOpenWithAffiliateProgramsClick

---
+++ src/main/java/testData/PositiveTestUserData.java
@@ -0,0 +1,8 @@
+package testData;
+
+public class PositiveTestUserData {
+    public static final String USERNAME = "QA3snetCO";
+    public static final String EMAIL = "QA3snetCO@gmail.com";
+    public static final String PASSWORD = "Aaa12345";
+
+}
