62f268f6e6bf1cfc5af9d8f0df33bac96a07e5f5
AIT TR Student <evgeniya@turtschin@gmail.com>
2025-07-07 15:15:11 +0200

    ChromeOptions argument updated for CI conflict resolve in GitHub Action

--- src/main/java/configuration/ApplicationManager.java
+++ src/main/java/configuration/ApplicationManager.java
@@ -25,9 +25,11 @@ public class ApplicationManager {
         } else if (browser.equals("chrome")) {
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("--lang=en");
-            chromeOptions.addArguments("--user-data-dir=/tmp/chrome-profile-" + UUID.randomUUID().toString()); // ← added for CI conflict in GitHub Action
+//            chromeOptions.addArguments("--user-data-dir=/tmp/chrome-profile-" + UUID.randomUUID().toString()); // ← added for CI conflict in GitHub Action
+            chromeOptions.addArguments("--headless=new");
             chromeOptions.addArguments("--no-sandbox");
             chromeOptions.addArguments("--disable-dev-shm-usage");
+            chromeOptions.addArguments("--disable-gpu");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver(chromeOptions);
         } else if (browser != null &&
