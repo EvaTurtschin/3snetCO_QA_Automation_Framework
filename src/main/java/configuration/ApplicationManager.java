9306bf76f0899d078d3e83915072327cca6ae609
EvaTurtschin <151837215+EvaTurtschin@users.noreply.github.com>
2025-07-10 13:04:14 +0200

    Update ApplicationManager.java
    - ApplicationManager updated (trying to handle window-size to maximum in CI)

--- src/main/java/configuration/ApplicationManager.java
+++ src/main/java/configuration/ApplicationManager.java
@@ -18,46 +18,45 @@ public class ApplicationManager {
     }
 
     public WebDriver init() {
-        if (browser.equals("edge")) {
-            EdgeOptions edgeOptions = new EdgeOptions();
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver(edgeOptions);
-        } else if (browser.equals("chrome")) {
-            ChromeOptions chromeOptions = new ChromeOptions();
-            System.out.println("Window size: " + driver.manage().window().getSize());
-            chromeOptions.addArguments("--lang=en");
+    if (browser.equals("edge")) {
+        EdgeOptions edgeOptions = new EdgeOptions();
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver(edgeOptions);
+    } else if (browser.equals("chrome")) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--lang=en");
 
-            String isCI = System.getenv("CI"); // GitHub Actions автоматически задаёт CI=true
+        boolean isCI = "true".equals(System.getenv("CI"));
 
-            if ("true".equals(isCI)) {
-                chromeOptions.addArguments("--headless=new");
-                chromeOptions.addArguments("--window-size=1920,1080");
-                chromeOptions.addArguments("--window-position=0,0");
-                chromeOptions.addArguments("--no-sandbox");
-                chromeOptions.addArguments("--disable-dev-shm-usage");
-                chromeOptions.addArguments("--disable-gpu");
-            } else {
-                chromeOptions.addArguments("--start-maximized");
-            }
-
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver(chromeOptions);
-              if ("true".equals(isCI)) {
-                  driver.manage().window().setSize(new Dimension(1920, 1080));
-              } else {
-                  driver.manage().window().maximize();
-              }
-        } else if (browser != null &&
-                !browser.equalsIgnoreCase("edge") &&
-                !browser.equalsIgnoreCase("chrome")) {
-            throw new IllegalArgumentException("Browser entered is not correct");
+        if (isCI) {
+            chromeOptions.addArguments("--headless=new");
+            chromeOptions.addArguments("--window-size=1920,1080");
+            chromeOptions.addArguments("--window-position=0,0");
+            chromeOptions.addArguments("--no-sandbox");
+            chromeOptions.addArguments("--disable-dev-shm-usage");
+            chromeOptions.addArguments("--disable-gpu");
+        } else {
+            chromeOptions.addArguments("--start-maximized");
         }
 
-        driver = new EventFiringDecorator(new WDListener()).decorate(driver);
-        driver.navigate().to("https://3snet.co/");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
 
-        return driver;
+        if (isCI) {
+            driver.manage().window().setSize(new Dimension(1920, 1080));
+        } else {
+            driver.manage().window().maximize();
+        }
+    } else {
+        throw new IllegalArgumentException("Unsupported browser: " + browser);
     }
+
+    System.out.println("Window size: " + driver.manage().window().getSize());
+
+    // driver = new EventFiringDecorator(new WDListener()).decorate(driver);
+    driver.navigate().to("https://3snet.co/");
+    return driver;
+}
     public void quit() {
         driver.quit();
     }
